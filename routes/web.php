835dbb0453aa18702e75fd8c7a7f9d659b89f117
Marcio Vieira <marcio.vieira@nooma.digital>
2023-09-04 21:55:37 -0300

    Initial Commit

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return 'Home';
+// });
+
+Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal']);
+Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class, 'sobreNos']);
+Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato']);
+Route::get('/login', function(){return 'Login';});
+
+Route::prefix('/app')->group(function(){
+    Route::get('/clientes', function(){return 'Clientes';});
+    Route::get('/fornecedores', function(){return 'Fornecedores';});
+    Route::get('/produtos', function(){return 'produtos';});
+});
